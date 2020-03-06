46e27f8a3e974321a4c05a5f4770c13a7bce75e1
Rodrigo <fuentesosorio@fuentesosorio.com>
2020-03-05 23:33:44 -0300

    Personajes page

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,13 @@
 
 window.onload = (function(){
 
+      let url = 'https://swapi.co/api/films';
+      let peliculas = document.querySelector("#home");
+      let urlpersonajes = "https://swapi.co/api/people";
+      let personajes = document.querySelector(".fondoPersonajes");
+
       const respira = document.querySelector(".breath");
+
       respira.addEventListener("mouseover", function(){
         let audio = document.querySelector("audio");
         audio.play();
@@ -10,8 +16,7 @@ window.onload = (function(){
 
       });
 
-      let url = 'https://swapi.co/api/films';
-      let peliculas = document.querySelector("#home");
+
 
       const getPeliculas = url => {
           fetch(url, {
@@ -44,16 +49,48 @@ window.onload = (function(){
               })
       }
 
+      const getPersonajes = urlpersonajes => {
+          fetch(urlpersonajes, {
+              method: 'GET', // GET, POST, PUT, DELETE
+              //body: JSON.stringify(data), // Solo en POST y PUT
+              headers: {
+                  'Content-Type': 'application/json'
+              }
+          })
+              .then(response => response.json())
+              .then(data => {
+                console.log(data);
+                  data.results.map(item => {
+
+                    personajes.innerHTML += `<div class="card">
+                          <img src="..." class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">${item.name}</h5>
+                            <p class="card-text">${item.opening_crawl}</p>
+                          </div>
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><strong>Director:</strong> ${item.director}</li>
+                            <li class="list-group-item"><strong>Productor:</strong> ${item.producer}</li>
+                            <li class="list-group-item"><small class=""><strong>Fecha de creación</strong> ${item.created}</small></li>
+                          </ul>
+
+                        </div>`;
+
+                  })
+
+              })
+      }
+
+      getPeliculas(url);
+      getPersonajes(urlpersonajes);
 
 
-
-
-
-          getPeliculas(url);
 });
 
-$(window).scroll(function(){
+
+
+/*$(window).scroll(function(){
       $(".fondo").css("opacity", 1 - $(window).scrollTop() / 500);
 
 
-    });
+    });*/
